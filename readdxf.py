b79e064f95a3638cc3c45caf29cb3a0e64e1b0c1
Roland Smith <rsmith@xs4all.nl>
2011-09-27 00:50:37 +0200

    Replace polylines with contours.

--- readdxf.py
+++ readdxf.py
@@ -3,7 +3,7 @@
 # Converts lines and arcs from a DXF file and prints them.
 #
 # Copyright © 2011 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
-# Time-stamp: <2011-09-26 17:49:15 rsmith>
+# Time-stamp: <2011-09-27 00:47:12 rsmith>
 # 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -31,17 +31,38 @@ import math
 
 
 # Class definitions
-
-delta=0.01
+DxfDelta=0.01
 
 class DxfEntity:
     '''A class for a line or arc entities'''
     def __init__(self):
-        self.xmin = 0.0
-        self.xmax = 0.0
-        self.xmax = 0.0
-        self.xmax = 0.0
-    def boundingbox(self):
+        # Start- and enpoint
+        self.x1 = self.x2 = 0.0
+        self.y1 = self.y2 = 0.0
+        # Bounding box
+        self.xmin = self.xmax = 0.0
+        self.xmax = self.xmax = 0.0
+    def fits(self, index, other):
+        if not isinstance(other, DxfEntity):
+            print "{} is not a DxfEntity!".format(other)
+            return 0
+        if index == 1:
+            if (math.fabs(self.x1-other.x1)<DxfDelta and 
+                math.fabs(self.y1-other.y1)<DxfDelta):
+                # return free end of other
+                return 2
+            elif (math.fabs(self.x1-other.x2)<DxfDelta and 
+                  math.fabs(self.y1-other.y2)<DxfDelta):
+                return 1
+        elif index == 2:
+            if (math.fabs(self.x2-other.x1)<DxfDelta and 
+                math.fabs(self.y2-other.y1)<DxfDelta):
+                return 2
+            elif (math.fabs(self.x2-other.x2)<DxfDelta and 
+                  math.fabs(self.y2-other.y2)<DxfDelta):
+                return 1
+        return 0 # doesn't fit!
+    def getbb(self):
         return (xmin, ymin, xmax, ymax)
 
 class DxfLine(DxfEntity):
@@ -57,6 +78,7 @@ class DxfLine(DxfEntity):
         self.x2 = float(elist[num])
         num = elist.index("21", num) + 1
         self.y2 = float(elist[num])
+        # Set bounding box
         if self.x2 > self.x1:
             self.xmin = self.x1
             self.xmax = self.x2
@@ -71,15 +93,11 @@ class DxfLine(DxfEntity):
             self.ymax = self.y1
     def swap(self):
         '''Swap (x1,y1) and (x2,y2)'''
-        tx = self.x1
-        ty = self.y1
-        self.x1 = self.x2
-        self.y1 = self.y2
-        self.x2 = tx
-        self.y2 = ty
+        (self.x1, self.x2) = (self.x2, self.x1)
+        (self.y1, self.y2) = (self.y2, self.y1)
     def __str__(self):
-        return "LINE from ({},{}) to ({},{})".format(self.x1, self.y1, 
-                                                    self.x2, self.y2)
+        fs = "LINE from ({:.3f},{:.3f}) to ({:.3f},{:.3f})"
+        return fs.format(self.x1, self.y1, self.x2, self.y2)
 
 class DxfArc(DxfEntity):
     '''A class for an arc entity, centering in (cx,cy) with radius R from angle
@@ -97,62 +115,88 @@ class DxfArc(DxfEntity):
         self.a1 = float(elist[num])
         num = elist.index("51", num) + 1
         self.a2 = float(elist[num])
-        self.x1 = self.cx+self.R*math.cos(math.radians(self.a1))
-        self.y1 = self.cy+self.R*math.sin(math.radians(self.a1))
-        self.x2 = self.cx+self.R*math.cos(math.radians(self.a2))
-        self.y2 = self.cy+self.R*math.sin(math.radians(self.a2))
-        # TODO: bounding-box calculation
+        if self.a2 < self.a1:
+            self.a2 += 360.0
+        # Start and endpoint 
+        self.x1 = self.xmin = self.cx+self.R*math.cos(math.radians(self.a1))
+        self.y1 = self.ymin = self.cy+self.R*math.sin(math.radians(self.a1))
+        self.x2 = self.xmax = self.cx+self.R*math.cos(math.radians(self.a2))
+        self.y2 = self.ymax = self.cy+self.R*math.sin(math.radians(self.a2))
+        # Refine bounding box
+        if self.xmin > self.xmax:
+            (self.xmin,self.xmax) = (self.xmax,self.xmin)
+        if self.ymin > self.ymax:
+            (self.ymin,self.ymax) = (self.ymax,self.ymin)
+        A1 = int(self.a1)/90
+        A2 = int(self.a2)/90
+        for ang in range(A1,A2):
+            (px,py) = (self.cx+self.R*math.cos(math.radians(90*ang)),
+                       self.cy+self.R*math.sin(math.radians(90*ang)))
+            if px > self.xmax:
+                self.xmax = px
+            elif px < self.xmin:
+                self.xmin = px
+            if py > self.ymax:
+                self.ymax = py
+            elif py < self.ymin:
+                self.ymin = py
     def startpoint(self):
         return (self.x1,self.y1)
     def endpoint(self):
         return (self.x2,self.y2)
     def __str__(self):
-        s = "ARC center ({},{}), radius {} from {} to {} degrees"
-        return s.format(self.cx, self.cy, self.R, self.a1, self.a2)
+        s = "ARC from ({:.3f},{:.3f}) to ({:.3f},{:.3f}), radius {:.3f}"
+        return s.format(self.x1, self.y1, self.x2, self.y2, self.R)
 
-class DxfPolyLine(DxfEntity):
-    '''A class for a poly line entity, from point (x1,y1) to (xn,yn)'''
-    def __init__(self, line):
-        '''Creates a polyline from an initial line segment.'''
-        self.points = [(line.x1,line.y1), (line.x2, line.y2)]
-        self.numpoints = 2
-    def append(self, line):
-        '''Appends line to the polyline, if one of the ends of line matches
-        the end of the polyline. Returns True if matched, otherwise False.'''
-        endpoint = self.points[-1]
-        if math.fabs(endpoint[0]-line.x1)<delta and math.fabs(endpoint[1]-line.y1)<delta:
-            self.points.append((line.x2,line.y2))
-            self.numpoints+=1
-            return True
-        if math.fabs(endpoint[0]-line.x2)<delta and math.fabs(endpoint[1]-line.y2)<delta:
-            self.points.append((line.x1,line.y1))
-            self.numpoints+=1
-            return True
-        return False
-    def prepend(self, line):
-        '''Prepends line to the polyline, if one of the ends of line matches
-        the start of the polyline. Returns True if matched, otherwise False.'''
-        endpoint = self.points[0]
-        if math.fabs(endpoint[0]-line.x1)<delta and math.fabs(endpoint[1]-line.y1)<delta:
-            self.points.insert(0, (line.x2,line.y2))
-            self.numpoints+=1
-            return True
-        if math.fabs(endpoint[0]-line.x2)<delta and math.fabs(endpoint[1]-line.y2)<delta:
-            self.points.insert(0, (line.x1,line.y1))
-            self.numpoints+=1
-            return True
-        return False
+
+class DxfContour:
+    '''A class for a list of connected DxfEntities'''
+    def __init__(self, ent):
+        '''Creates a contour from an initial entity.'''
+        self.ent = [ent]
+        self.nument = 1
+        self.firstfree = 1
+        self.lastfree = 2
+    def append(self, ent):
+        '''Appends and entity to the contour, if one of the ends of entity
+        matches the end of the last entity. Returns True if matched, otherwise
+        False.'''
+        last = self.ent[-1]
+        newfree = last.fits(self.lastfree, ent)
+        if newfree == 0:
+            return False
+        self.ent.append(ent)
+        self.nument += 1
+        self.lastfree = newfree
+        if newfree == 1 and isinstance(ent, DxfLine):
+            newfree = 2;
+            ent.swap()
+        self.firstfree = newfree
+        return True
+    def prepend(self, ent):
+        '''Prepends and entity to the contour, if one of the ends of entity
+        matches the end of the first entity. Returns True if matched,
+        otherwise False.'''
+        first = self.ent[0]
+        newfree = first.fits(self.firstfree, ent)
+        if newfree == 0:
+            return False
+        self.ent.insert(0,ent)
+        self.nument += 1
+        if newfree == 2 and isinstance(ent, DxfLine):
+            newfree = 1;
+            ent.swap()
+        self.firstfree = newfree
+        return True
     def __str__(self):
-        outstr = "#Polyline\n"
-        for n in range(0,self.numpoints-1):
-            outstr +=  "|LINE from ({},{}) to ({},{})\n".format(self.points[n][0], 
-                                                                self.points[n][1], self.points[n+1][0], 
-                                                                self.points[n+1][1])
+        outstr = "#Contour\n"
+        for e in self.ent:
+            outstr += "|" + str(e) + "\n"
         return outstr[0:-1]
 
 # Function definitions.
 
-def opendxf(name):
+def readdxfentities(name):
     '''Opens the DXF file 'name', and return a list of entities'''
     dxffile = open(name)
     sdata = [str.strip() for str in dxffile.readlines()]
@@ -164,44 +208,47 @@ def opendxf(name):
     del sdata
     return entities
 
-def findent(ename, ent):
+def findent(ename, el):
     '''Searches the ent list for the entity named in the ename string. Returns a list of indices.'''
-    cnt = ent.count(ename)
+    cnt = el.count(ename)
     if cnt > 0:
-        return [x for x in range(len(ent)) if ent[x] == ename]
+        return [x for x in range(len(el)) if el[x] == ename]
     return []
 
-def findpolylines(lol):
-    '''Find polylines in the list of lines lol. Returns a list of polylines
-    and a list of remaining lines as a tuple.'''
-    lines = lol[:]
-    rl = []
-    lop = []
-    while len(lines) > 0:
-        first = lines.pop(0)
-        pl = DxfPolyLine(first)
+def findcontours(lol, loa):
+    '''Find polylines in the list of lines loe and list of arcs loa. Returns a
+    list of contours and a list of remaining lines and a list of remaining
+    arcs as a tuple.'''
+    remlines = []
+    remarcs = []
+    elements = lol[:]+loa[:]
+    loc = []
+    while len(elements) > 0:
+        first = elements.pop(0)
+        cn = DxfContour(first)
         n = 0
-        while n < len(lines):
-            if pl.append(lines[n]) or pl.prepend(lines[n]):
-                lines.pop(n)
+        while n < len(elements):
+            if cn.append(elements[n]) or cn.prepend(elements[n]):
+                del elements[n]
             else:
                 n += 1
-        if pl.numpoints < 3:
-            rl.append(first)
+        if cn.nument > 1:
+            loc.append(cn)
         else:
-            lop.append(pl)
-            #print "# Found polyline of {} points.".format(pl.numpoints)
-    return (rl,lop)
+            if isinstance(first, DxfLine):
+                remlines.append(first)
+            elif isinstance(first, DxfArc):
+                remarcs.append(first)
+    return (loc, remlines, remarcs)
 
 # Main program starts here.
 if len(sys.argv) == 1:
     print "Usage: {} dxf-file(s)".format(sys.argv[0])
     exit(1)
-
 del sys.argv[0]
 for f in sys.argv:
     # Find entities
-    ent = opendxf(f)
+    ent = readdxfentities(f)
     lo = findent("LINE", ent)
     lines = []
     if len(lo) > 0:
@@ -210,12 +257,12 @@ for f in sys.argv:
     arcs = []
     if len(ao) > 0:
         arcs = [DxfArc(ent,m) for m in ao]
-    # Find polylines
-    (remlines,polylines) = findpolylines(lines)
+    # Find contours
+    (contours,remlines,remarcs) = findcontours(lines, arcs)
     # Output
-    for p in polylines:
-        print p
+    for c in contours:
+        print c
     for l in remlines:
         print l
-    for a in arcs:
+    for a in remarcs:
         print a
