fe9c22fc4c2bf2f3be25a401eb1084d2b255286e
Roland Smith <rsmith@xs4all.nl>
2013-07-17 02:15:20 +0200

    Re-implement dxf2nc for the largest part.

--- dxf2nc.py
+++ dxf2nc.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Optimizes lines and arcs from a DXF file for cutting on a Gerber cutter,
-# outputs G-codes for the Gerber cutter..
+# outputs G-codes for the Gerber cutter.
 #
-# Copyright © 2012, 2013 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
+# Copyright © 2012,2013 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
 # $Date$
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -27,86 +27,119 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 
-import sys
+import sys 
 import nctools.dxf as dxf
-from nctools.utils import outname
+import nctools.ent as ent
+import nctools.bbox as bbox
+import nctools.utils as utils
+import nctools.gerbernc as gerbernc
 
-__proginfo__ = ('dxf2nc [ver. ' + '$Revision$'[11:-2] + 
-                '] ('+'$Date$'[7:-2]+')')
+__proginfo__ = ('dxf2nc [ver. ' + 
+                '$Revision$'[11:-2] + '] (' + 
+                '$Date$'[7:-2]+')')
 
 
-def nc_header(progname, bbox):
-    """Returns the start of the NC file.
-    
-    :progname: name of the file
-    :bbox: 4-tuple (xmin, ymin, xmax, ymax) in mm
-    :returns: a string containing the header for the nc file. 
+def _cutline(e, wr):
+    """Cut a ent.Line
+
+    :ent: nctools.ent.Line
+    :wr: nctoos.gerbernc.Writer
     """
-    Li = (bbox[2]-bbox[0])/25.4
-    Wi = (bbox[3]-bbox[1])/25.4
-    s = "H1*M20*{}/L={:5.3f}/W={:5.3f}*N1*M15*".format(progname, Li, Wi)
-    return s
+    wr.moveto(e.x[0], e.y[0])
+    wr.down()
+    wr.moveto(e.x[1], e.y[1])
+    wr.up()
 
+def _cutarc(e, wr):
+    """Cut an ent.Arc
 
-def nc_footer():
-    """Returns the ending of the NC file."""
-    return 'M0*'
+    :ent: nctools.ent.Arc
+    :wr: nctoos.gerbernc.Writer
+    """
+    pnts = e.segments()
+    x, y = pnts.pop(0)
+    wr.moveto(x, y)
+    wr.down()
+    for x, y in pnts:
+        wr.moveto(x, y)
+    wr.up()
 
+def _cutpoly(e, wr):
+    """Cut a ent.Polyline
 
-def main(argv): #pylint: disable=R0912
+    :ent: nctools.ent.Polyline
+    :wr: nctoos.gerbernc.Writer
+    """
+    wr.down()
+    # not implemented yet
+    wr.up()
+
+def _cutcontour(e, wr):
+    """Cut a ent.Contour
+
+    :ent: nctools.ent.Contour
+    :wr: nctoos.gerbernc.Writer
+    """
+    wr.moveto(e.entities[0].x[0], e.entities[0].y[0])
+    wr.down()
+    for ce in e.entities[1:]:
+        if isinstance(ce, ent.Arc):
+            pnts = e.segments()
+            pnts.pop(0)
+            for x, y in pnts:
+                wr.moveto(x, y)
+        elif isinstance(ce, ent.Line):
+            wr.moveto(e.x[1], e.y[1])
+    wr.up()
+
+def main(argv):
     """Main program for the dxf2nc utility.
-
+    
     :argv: command line arguments
-    """    
+    """
     if len(argv) == 1:
         print __proginfo__
-        print "Usage: {} [file.dxf ...]".format(argv[0])
-        exit(1)
+        print "Usage: {} [dxf-file ...]".format(argv[0])
+        sys.exit(1)
     del argv[0]
     for f in argv:
         try:
-            ofn = outname(f, extension='.nc')
-            # Find entities
+            ofn = utils.outname(f, extension='.nc')
             entities = dxf.Reader(f)
-        except ValueError:
-            fns = "Cannot construct output filename. Skipping file '{}'."
-            print fns.format(f)
-            print "A valid filename _must_ have a '.dxf' extension."
-            print "And it must be more than just the extension."
+        except Exception as e: #pylint: disable=W0703
+            utils.skip(e, f)
             continue
-        except IOError as e:
-            print "Cannot read file: {}".format(e)
-            print "Skipping file '{}'".format(f)
-            continue
-        if len(entities) == 0:
-            print "No lines or arcs found. Skipping file '{}'".format(f)
-            continue
-        print 'Found {} entities.'.format(len(entities))
-        # Move bounding box of entities to 0,0.
-        bbs = [e.getbb() for e in entities]
-        bb = bbs[0]
-        for add in bbs[1:]:
-            bb = dxfgeom.merge_bb(bb, add)
-        dispx = -bb[0]
-        dispy = -bb[1]
-        for e in entities:
-            e.move(dispx, dispy)
-        # Find contours
-        print 'Looking for contours...'
-        (contours, rement) = dxfgeom.find_contours(entities)
-        print 'Found {} contours.'.format(len(contours))
-        print '{} entities remain'.format(len(rement))
-        entities = contours + rement
-        entities.sort()
-        # Output
-        outf = open(ofn, 'w')
-        outf.write(nc_header(ofn[:-3], bb))
-        for e in entities:
-            s1, s2 = e.ncdata()
-            outf.write(s1+s2)
-        outf.write(nc_footer())
-        outf.close()
+        num = len(entities)
+        print 'Filename:', f
+        if num == 0:
+            print 'No entities found!'
+            sys.exit(1)
+        if num > 1:
+            print 'Contains: {} entities'.format(num)
+            bbe = [e.bbox for e in entities]
+            bb = bbox.merge(bbe)
+            contours, rement = ent.findcontours(entities)
+            ncon = 'Found {} contours, {} remaining single entities'
+            print ncon.format(len(contours), len(rement))
+            entities = contours + rement
+        else:
+            print 'Contains: 1 entity'
+            bb = entities[0].bbox
+        es = 'Extents: {:.1f} ≤ x ≤ {:.1f}, {:.1f} ≤ y ≤ {:.1f}'
+        print es.format(bb.minx, bb.maxx, bb.miny, bb.maxy)
+        length = sum(e.length for e in entities)
+        print 'Total length of entities: {:.0f} mm'.format(length)
+        with gerbernc.Writer(ofn) as w:
+            for e in entities:
+                if isinstance(e, ent.Contour):
+                    _cutcontour(e, w)
+                elif isinstance(e, ent.Polyline):
+                    _cutpoly(e, w)
+                elif isinstance(e, ent.Arc):
+                    _cutarc(e, w)
+                elif isinstance(e, ent.Line):
+                    _cutline(e, w)
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main(utils.xpand(sys.argv))
