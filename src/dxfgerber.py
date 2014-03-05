9599440c55578da49f619bf47f862ad651a92e4d
Roland Smith <rsmith@xs4all.nl>
2014-03-05 20:11:22 +0100

    Reworked dxfgerber.

--- src/dxfgerber.py
+++ src/dxfgerber.py
@@ -1,40 +1,49 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Copyright © 2011-2013 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
+# dxfgerber - main program
+# vim:fileencoding=utf-8
 # $Date$
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-# SUCH DAMAGE.
 
 """Reads DXF files and re-orders the entities so that entities that fit
 together are stored as a chain in the output DXF file."""
+
+from __future__ import print_function, division
+
+__version__ = '$Revision$'[11:-2]
+
+_lic = """dxfgerber {}
+Copyright © 2011-2014 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
+$Date$
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.""".format(__version__)
+
 
 import argparse
 import sys
 from nctools import bbox, dxf, ent, utils
 
-_proginfo = ('dxfgerber [ver. ' +
-             '$Revision$'[11:-2] + '] (' +
-             '$Date$'[7:-2]+')')
+
+class LicenseAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        print(_lic)
+        sys.exit()
 
 
 def main(argv):
@@ -48,8 +57,11 @@ def main(argv):
     searching for contours (defaults to 0.5 mm)"""
     parser.add_argument('-l', '--limit', nargs=1, help=argtxt, dest='limit',
                         metavar='F', type=float, default=0.5)
-    parser.add_argument('-v', '--version', action='version',
-                        version=_proginfo)
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-L', '--license', action=LicenseAction, nargs=0,
+                       help="print the license")
+    group.add_argument('-v', '--version', action='version',
+                       version=__version__)
     parser.add_argument('files', nargs='*', help='one or more file names',
                         metavar='file')
     pv = parser.parse_args(argv)
