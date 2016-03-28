f17a2329d6a496da4f394c1c49b78f5174aeb26c
Roland Smith <rsmith@xs4all.nl>
2016-03-28 02:40:47 +0200

    Start adding unit tests.

---
+++ tests/pathmagic.py
@@ -0,0 +1,15 @@
+# file: pathmagic.py
+# vim:fileencoding=utf-8:ft=python
+#
+# Author: R.F. Smith <rsmith@xs4all.nl>
+# Created: 2016-03-28 01:55:57 +0200
+# Last modified: 2016-03-28 02:20:16 +0200
+
+"""Path hack to make tests work."""
+
+import os
+import sys
+
+bp = os.path.dirname(os.path.realpath('.')).split(os.sep)
+modpath = os.sep.join(bp + ['src', 'nctools'])
+sys.path.insert(0, modpath)
---
+++ tests/test-dxfreader.py
@@ -0,0 +1,52 @@
+# file: test-dxfreader.py
+# vim:fileencoding=utf-8:ft=python
+#
+# Author: R.F. Smith <rsmith@xs4all.nl>
+# Created: 2016-03-28 01:44:10 +0200
+# Last modified: 2016-03-28 02:39:35 +0200
+
+"""Tests for the dxfreader module.
+
+Run from the tests directory with: nosetests-3.5 -v test-dxfreader.py"""
+
+import pathmagic
+import dxfreader
+
+data = []
+ents = []
+names = []
+
+
+def test_parse():
+    global data
+    data = dxfreader.parse('../testfiles/demo.dxf')
+    assert len(data) == 4768
+    assert data[0] == (0, 'SECTION')
+    assert data[-1] == (0, 'EOF')
+
+
+def test_entities():
+    global ents
+    ents = dxfreader.entities(data)
+    start = ((0, 'LINE'), (5, '359'), (330, '475'), (100, 'AcDbEntity'),
+             (8, 'deel 1'), (100, 'AcDbLine'), (10, '999.9999999999984'),
+             (20, '100.0'), (30, '0.0'), (11, '1100.0'), (21, '100.0'),
+             (31, '0.0'))
+    end = ((0, 'LINE'), (5, '37A'), (330, '475'), (100, 'AcDbEntity'),
+           (8, 'deel 1'), (100, 'AcDbLine'), (10, '999.9999999999982'),
+           (20, '575.0'), (30, '0.0'), (11, '999.9999999999982'),
+           (21, '600.0'), (31, '0.0'))
+    assert len(ents) == 16
+    assert ents[0] == start
+    assert ents[-1] == end
+
+
+def test_layernames():
+    global names
+    names = dxfreader.layernames(ents)
+    assert names == ['deel 1']
+
+
+def test_numberedlayers():
+    numnames = dxfreader.numberedlayers(ents)
+    assert numnames == ['deel 1']
