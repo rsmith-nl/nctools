6270a5bf25c291718ace1775a7cb6ba56a670bc8
Roland Smith <rsmith@xs4all.nl>
2018-01-23 00:01:07 +0100

    Start re-tooling for setuptools with entry_points.

--- src/nc2pdf.py
+++ src/nc2pdf.py
@@ -1,16 +1,21 @@
+#!/usr/bin/env python3
+# file: nc2pdf.py
+# vim:fileencoding=utf-8:fdm=marker:ft=python
 # nc2pdf - main program
-# vim:fileencoding=utf-8
+#
+# Author: R.F. Smith <rsmith@xs4all.nl>
+# Created: 2016-02-19 22:34:29 +0100
+# Last modified: 2018-01-22 23:58:52 +0100
 """Plot cuts from a Gerber cloth cutter NC file to a PDF."""
 
 import argparse
 import logging
 import sys
 from nctools import gerbernc, plot, utils
-
-__version__ = '2-beta'
+from nctools import __version__
 
 _lic = """nc2pdf {}
-Copyright © 2013, 2016 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
+Copyright © 2013, 2016, 2018 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
@@ -40,56 +45,64 @@ class LicenseAction(argparse.Action):
         sys.exit()
 
 
-parser = argparse.ArgumentParser(description=__doc__)
-group = parser.add_mutually_exclusive_group()
-group.add_argument(
-    '-L', '--license', action=LicenseAction, nargs=0, help="print the license")
-group.add_argument('-v', '--version', action='version', version=__version__)
-parser.add_argument(
-    '--log',
-    default='warning',
-    choices=['debug', 'info', 'warning', 'error'],
-    help="logging level (defaults to 'warning')")
-parser.add_argument(
-    'files', nargs='*', help='one or more file names', metavar='file')
-args = parser.parse_args(sys.argv[1:])
-logging.basicConfig(
-    level=getattr(logging, args.log.upper(), None),
-    format='%(levelname)s: %(message)s')
-logging.debug('command line arguments = {}'.format(sys.argv))
-logging.debug('parsed arguments = {}'.format(args))
-if not args.files:
-    parser.print_help()
-    sys.exit(0)
-for fn in utils.xpand(args.files):
-    logging.info('starting file "{}"'.format(fn))
-    try:
-        ofn = utils.outname(fn, extension='.pdf', addenum='_nc')
-        cuts = list(gerbernc.segments(fn))
-    except ValueError as e:
-        logging.info(str(e))
-        fns = "cannot construct output filename. Skipping file '{}'."
-        logging.error(fns.format(fn))
-        continue
-    except IOError as e:
-        logging.info("Cannot read file: {}".format(e))
-        logging.error("i/o error, skipping file '{}'".format(fn))
-        continue
-    cnt = len(cuts)
-    logging.info('got {} cuts'.format(cnt))
-    xvals = [pnt[0] for s in cuts for pnt in s]
-    yvals = [pnt[1] for s in cuts for pnt in s]
-    minx, maxx = min(xvals), max(xvals)
-    miny, maxy = min(yvals), max(yvals)
-    bs = '{} range from {:.1f} mm to {:.1f} mm'
-    logging.info(bs.format('X', minx, maxx))
-    logging.info(bs.format('Y', miny, maxy))
-    logging.info('plotting the cuts')
-    out, ctx = plot.setup(ofn, minx, miny, maxx, maxy)
-    plot.grid(ctx, minx, miny, maxx, maxy)
-    plot.lines(ctx, cuts)
-    plot.title(ctx, 'nc2pdf', ofn, maxy - miny)
-    out.show_page()
-    logging.info('Writing output file "{}"'.format(ofn))
-    out.finish()
-    logging.info('File "{}" done.'.format(fn))
+def main():
+    """
+    Entry point for nc2pdf.py.
+    """
+    parser = argparse.ArgumentParser(description=__doc__)
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        '-L', '--license', action=LicenseAction, nargs=0, help="print the license")
+    group.add_argument('-v', '--version', action='version', version=__version__)
+    parser.add_argument(
+        '--log',
+        default='warning',
+        choices=['debug', 'info', 'warning', 'error'],
+        help="logging level (defaults to 'warning')")
+    parser.add_argument(
+        'files', nargs='*', help='one or more file names', metavar='file')
+    args = parser.parse_args(sys.argv[1:])
+    logging.basicConfig(
+        level=getattr(logging, args.log.upper(), None),
+        format='%(levelname)s: %(message)s')
+    logging.debug('command line arguments = {}'.format(sys.argv))
+    logging.debug('parsed arguments = {}'.format(args))
+    if not args.files:
+        parser.print_help()
+        sys.exit(0)
+    for fn in utils.xpand(args.files):
+        logging.info('starting file "{}"'.format(fn))
+        try:
+            ofn = utils.outname(fn, extension='.pdf', addenum='_nc')
+            cuts = list(gerbernc.segments(fn))
+        except ValueError as e:
+            logging.info(str(e))
+            fns = "cannot construct output filename. Skipping file '{}'."
+            logging.error(fns.format(fn))
+            continue
+        except IOError as e:
+            logging.info("Cannot read file: {}".format(e))
+            logging.error("i/o error, skipping file '{}'".format(fn))
+            continue
+        cnt = len(cuts)
+        logging.info('got {} cuts'.format(cnt))
+        xvals = [pnt[0] for s in cuts for pnt in s]
+        yvals = [pnt[1] for s in cuts for pnt in s]
+        minx, maxx = min(xvals), max(xvals)
+        miny, maxy = min(yvals), max(yvals)
+        bs = '{} range from {:.1f} mm to {:.1f} mm'
+        logging.info(bs.format('X', minx, maxx))
+        logging.info(bs.format('Y', miny, maxy))
+        logging.info('plotting the cuts')
+        out, ctx = plot.setup(ofn, minx, miny, maxx, maxy)
+        plot.grid(ctx, minx, miny, maxx, maxy)
+        plot.lines(ctx, cuts)
+        plot.title(ctx, 'nc2pdf', ofn, maxy - miny)
+        out.show_page()
+        logging.info('Writing output file "{}"'.format(ofn))
+        out.finish()
+        logging.info('File "{}" done.'.format(fn))
+
+
+if __name__ == '__main__':
+    main()
--- src/readdxf.py
+++ src/readdxf.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python3
+# file: readdxf.py
+# vim:fileencoding=utf-8:fdm=marker:ft=python
 # readdxf - main program
-# vim:fileencoding=utf-8
+#
+# Copyright © 2018 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
+# Created: 2016-02-19 22:34:29 +0100
+# Last modified: 2018-01-22 23:56:43 +0100
 """Reads DXF files and prints the entities in human-readable form."""
 
 import argparse
@@ -9,10 +15,10 @@ import pprint
 import sys
 from nctools import dxfreader as dx
 from nctools import utils as ut
+from nctools import __version__
 
-__version__ = '2-beta'
 _lic = """readdxf {}
-Copyright © 2011-2016 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
+Copyright © 2011-2018 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
@@ -95,56 +101,61 @@ def printent(e, v):
             print('  {} entity'.format(k))
 
 
-parser = argparse.ArgumentParser(description=__doc__)
-argtext1 = 'show details of unknown entities'
-parser.add_argument('-v', '--verbose', help=argtext1, action="store_true")
-parser.add_argument(
-    '-a',
-    '--all',
-    action="store_true",
-    help='process all layers (default: numbered layers)')
-parser.add_argument(
-    '--log',
-    default='warning',
-    choices=['debug', 'info', 'warning', 'error'],
-    help="logging level (defaults to 'warning')")
-group = parser.add_mutually_exclusive_group()
-group.add_argument(
-    '-L', '--license', action=LicenseAction, nargs=0, help="print the license")
-group.add_argument('-V', '--version', action='version', version=__version__)
-parser.add_argument(
-    'files', metavar='file', nargs='*', help='one or more file names')
-args = parser.parse_args(sys.argv[1:])
-logging.basicConfig(
-    level=getattr(logging, args.log.upper(), None),
-    format='%(levelname)s: %(message)s')
-logging.debug('Command line arguments = {}'.format(sys.argv[1:]))
-logging.debug('Parsed arguments = {}'.format(args))
-if not args.files:
-    parser.print_help()
-    sys.exit(0)
-for f in ut.xpand(args.files):
-    print('Filename: {}'.format(f))
-    try:
-        data = dx.parse(f)
-        if args.verbose:
-            for d in data:
-                pprint.pprint(d)
-        entities = dx.entities(data)
-    except Exception as ex:
-        ut.skip(ex, f)
-        continue
-    if not args.all:
-        numbered = dx.numberedlayers(entities)
-        bylayer = {nm: dx.fromlayer(entities, nm) for nm in numbered}
-        entities = []
-        for layerent in bylayer.values():
-            entities += layerent
-    num = len(entities)
-    if num == 0:
-        logging.warning('no entities found!')
-        continue
-    else:
+def process_arguments():
+    parser = argparse.ArgumentParser(description=__doc__)
+    argtext1 = 'show details of unknown entities'
+    parser.add_argument('-v', '--verbose', help=argtext1, action="store_true")
+    parser.add_argument(
+        '-a',
+        '--all',
+        action="store_true",
+        help='process all layers (default: numbered layers)')
+    parser.add_argument(
+        '--log',
+        default='warning',
+        choices=['debug', 'info', 'warning', 'error'],
+        help="logging level (defaults to 'warning')")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        '-L', '--license', action=LicenseAction, nargs=0, help="print the license")
+    group.add_argument('-V', '--version', action='version', version=__version__)
+    parser.add_argument(
+        'files', metavar='file', nargs='*', help='one or more file names')
+    args = parser.parse_args(sys.argv[1:])
+    logging.basicConfig(
+        level=getattr(logging, args.log.upper(), None),
+        format='%(levelname)s: %(message)s')
+    logging.debug('Command line arguments = {}'.format(sys.argv[1:]))
+    logging.debug('Parsed arguments = {}'.format(args))
+    return args
+
+
+def main():
+    """
+    Entry point for readdxf.py.
+    """
+    args = process_arguments()
+    for f in ut.xpand(args.files):
+        print('Filename: {}'.format(f))
+        try:
+            data = dx.parse(f)
+            if args.verbose:
+                for d in data:
+                    pprint.pprint(d)
+            entities = dx.entities(data)
+        except Exception as ex:
+            logging.info('skipping file {}: {}'.format(f, ex))
+            continue
+        if not args.all:
+            numbered = dx.numberedlayers(entities)
+            bylayer = {nm: dx.fromlayer(entities, nm) for nm in numbered}
+            entities = []
+            for layerent in bylayer.values():
+                entities += layerent
+        num = len(entities)
+        if num == 0:
+            logging.warning('no entities found!')
+            continue
         print('Contains: {} entities'.format(num))
         if args.verbose:
             for e in entities:
@@ -154,3 +165,7 @@ for f in ut.xpand(args.files):
             print('Layer: "{}"'.format(layer))
             for e in dx.fromlayer(entities, layer):
                 printent(e, args.verbose)
+
+
+if __name__ == '__main__':
+    main()
